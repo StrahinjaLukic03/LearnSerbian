46fd148c916db5195a2269e5d8d4870622896603
Strahinja <strahinjalukic03@gmail.com>
2021-12-20 20:41:18 +0100

    With first exercise and rough home and information page

---
+++ scripts/enterKey.js
@@ -0,0 +1,4 @@
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13)
+        document.getElementById("panelButton").click();
+});
---
+++ scripts/letterButtons.js
@@ -0,0 +1,53 @@
+function ch() {
+    var inputid = document.getElementById("answer");
+    var inputvalue = document.getElementById("answer").value;
+    
+    inputid.value = inputvalue + "č";
+    
+    document.getElementById("answer").focus();
+}
+
+function tj() {
+    var inputid = document.getElementById("answer");
+    var inputvalue = document.getElementById("answer").value;
+    
+    inputid.value = inputvalue + "ć";
+
+    document.getElementById("answer").focus();
+}
+
+function dj() {
+    var inputid = document.getElementById("answer");
+    var inputvalue = document.getElementById("answer").value;
+    
+    inputid.value = inputvalue + "đ";
+
+    document.getElementById("answer").focus();
+}
+
+function sh() {
+    var inputid = document.getElementById("answer");
+    var inputvalue = document.getElementById("answer").value;
+    
+    inputid.value = inputvalue + "š";
+
+    document.getElementById("answer").focus();
+}
+
+function zh() {
+    var inputid = document.getElementById("answer");
+    var inputvalue = document.getElementById("answer").value;
+    
+    inputid.value = inputvalue + "ž";
+
+    document.getElementById("answer").focus();
+}
+
+function dzh() {
+    var inputid = document.getElementById("answer");
+    var inputvalue = document.getElementById("answer").value;
+    
+    inputid.value = inputvalue + "dž";
+
+    document.getElementById("answer").focus();
+}
